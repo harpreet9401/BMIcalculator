a996b0c4891486fd55ffc422184af15ea29a967a
Harpreet Kaur <Harpreet@Gurpreets-MacBook-Air.local>
2022-01-02 21:21:59 +0530

    Bind seekbar value to height textview.

--- app/src/main/java/com/harpreet/bmicalculator/MainActivity.java
+++ app/src/main/java/com/harpreet/bmicalculator/MainActivity.java
@@ -3,6 +3,9 @@ package com.harpreet.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView height = findViewById(R.id.selected_height_textview);
+        SeekBar seekbar = findViewById(R.id.seekbar);
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                height.setText(progress + " cm");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // No-Op
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // No-Op
+            }
+        });
     }
 }
